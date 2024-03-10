9ceb18856d0d373c992b78a7ca3f1fa75a9abb01
Daniel Bury <danielbury@Daniels-MBP.lan>
2024-03-10 13:06:13 -0700

    feat(loading): add simple loading for async state functions

--- pages/profile/fullfacerub.js
+++ pages/profile/fullfacerub.js
@@ -17,6 +17,10 @@ const Profile = () => {
     // getFeedData('@fullfacerub');
   }, [])
 
+  if (!profileData.handle) {
+    return <p>...Loading</p>
+  }
+
   return (
   <div style={{
     display: 'flex',
